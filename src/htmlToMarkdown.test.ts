7844c1129d46fd793e863ea8053368173b5484db
Josh Pollock <josh@calderawp.com>
2019-09-30 14:39:45 -0400

    minor typescript fixes

--- src/htmlToMarkdown.test.ts
+++ src/htmlToMarkdown.test.ts
@@ -1,18 +1,17 @@
-const htmlToMarkdown = require( './htmlToMarkdown');
+const htmlToMarkdown = require('./htmlToMarkdown');
 
-it( 'converts', async () => {
-     const contents = await htmlToMarkdown('<div><h1>Heading</h1><p>contents</p><p>Other P</p></div>');
-     expect(contents).toContain('# Heading');
-     expect(contents).toContain('contents');
-
-
+it('converts', async () => {
+  const contents = await htmlToMarkdown(
+    '<div><h1>Heading</h1><p>contents</p><p>Other P</p></div>'
+  );
+  expect(contents).toContain('# Heading');
+  expect(contents).toContain('contents');
 });
 
+it('converts links', async () => {
+  const contents = await htmlToMarkdown(
+    '<div><a href="https://hiroy.club">Say Hi Roy</a>Not in link</div>'
+  );
 
-it( 'converts links' , async () => {
-    const contents = await htmlToMarkdown('<div><a href="https://hiroy.club">Say Hi Roy</a>Not in link</div>' );
-   
-    expect(contents).toContain( '[Say Hi Roy](https://hiroy.club)')
-
-
+  expect(contents).toContain('[Say Hi Roy](https://hiroy.club)');
 });
