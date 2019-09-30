48be5f4ffea744b1df0e60b80b678fd78590a8a5
Josh Pollock <josh@calderawp.com>
2019-09-30 15:14:57 -0400

    test to prove can get page or posts

--- src/wpToStatic.test.ts
+++ src/wpToStatic.test.ts
@@ -4,6 +4,16 @@ test( 'Limits per page', async() => {
     expect( data.length ).toBe(5);
 });
 
+test( 'Gets the right post type', async ( ) => {
+    const pages = await wpToStatic( 'https://calderaforms.com/wp-json',5,2,'page' );
+    const posts = await wpToStatic( 'https://calderaforms.com/wp-json',5,1 );
+
+    expect( posts[0].type ).toEqual('post');
+    expect( pages[0].type ).toEqual('page');
+
+
+});
+
 test( 'Paginates', async() => {
     const dataPage2 = await wpToStatic( 'https://calderaforms.com/wp-json',5,2 );
     const dataPage1 = await wpToStatic( 'https://calderaforms.com/wp-json',5,1 );
--- src/wpToStatic.ts
+++ src/wpToStatic.ts
@@ -1,12 +1,21 @@
 const WPAPI = require( 'wpapi' );
 const fs = require( 'fs');
-module.exports = async function wpToStatic(endpoint: String,perPage: Number,page: Number){
-    const wp = new WPAPI({ endpoint: endpoint});
+module.exports = async function wpToStatic(endpoint: String,perPage: Number,page: Number,postType: string = 'post'){
+    let wp = new WPAPI({ endpoint: endpoint});
     return new Promise( (resolve,reject) => {
-        wp
-            .posts()
-            .page(page)
-            .perPage(perPage)
+
+
+        switch( postType ){
+            case 'page':
+               wp = wp.pages();
+                break;
+            default:
+                wp = wp.posts();
+                break;
+        }
+        
+            wp.perPage( perPage ).page( page )
+        
             .get(function(  err: Error, data: Array<{
             content: {
                 rendered: String
@@ -16,7 +25,8 @@ module.exports = async function wpToStatic(endpoint: String,perPage: Number,page
             },
             slug: String,
             link: String
-            id: Number
+            id: Number,
+            type: String
         }> ) {
             if ( err ) {
                 reject( err );
