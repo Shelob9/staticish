9e81a69e679d32c1d5194328f0dfa9cc2f059400
Josh Pollock <josh@calderawp.com>
2019-09-30 14:40:31 -0400

    Clean up spacing a bit

--- src/htmlToMarkdown.ts
+++ src/htmlToMarkdown.ts
@@ -1,26 +1,20 @@
-const unified = require('unified')
-const createStream = require('unified-stream')
-const parse = require('rehype-parse')
-const rehype2remark = require('rehype-remark')
-const stringify = require('remark-stringify')
+const unified = require('unified');
+const parse = require('rehype-parse');
+const rehype2remark = require('rehype-remark');
+const stringify = require('remark-stringify');
 
 const processor = unified()
   .use(parse)
   .use(rehype2remark)
-  .use(stringify)
+  .use(stringify);
 
-
-module.exports = async function htmlToMarkdown(content: String){
-  return new Promise( (resolve,reject) => {
-    processor.process(content, (err: Error,file:{
-      contents: String
-    }) => {
-        if( err ){
-          reject(err);
-        }
-        resolve(file.contents);
-
+module.exports = async function htmlToMarkdown(content: String) {
+  return new Promise((resolve, reject) => {
+    processor.process(content, (err: Error, file: { contents: String }) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(file.contents);
     });
   });
-   
-}
+};
