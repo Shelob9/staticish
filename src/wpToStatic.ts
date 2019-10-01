b00eab14c8df26bf0773ebc83fd0acecf442bd0f
Josh Pollock <josh@calderawp.com>
2019-10-01 10:56:31 -0400

    remove usless abstraction

--- src/wpToStatic.ts
+++ src/wpToStatic.ts
@@ -21,41 +21,6 @@ type contentArgs = {
 };
 
 
-//REMOVE THIS USELESS ABSTRACTION
-async function getContent(args: contentArgs) : Promise<Array<Post>>{
-    const {
-        endpoint,
-        perPage,
-    } = args;
-    const page = args.page ? args.page : 1;
-    const postType = args.postType ? args.postType  : 'post';
-    let wp = new WPAPI({ endpoint: endpoint});
-    return new Promise( (resolve,reject) => {
-
-        switch( postType ){
-            case 'page':
-               wp = wp.pages();
-                break;
-            default:
-                wp = wp.posts();
-                break;
-        }
-        
-            wp.
-            perPage( perPage )
-            .page( page )
-        
-            .get(function(  err: Error, data: Array<Post> ) {
-            if ( err ) {
-                reject( err );
-            }
-        
-            resolve(data);
-
-        });
-    });
-}
-
 
 type writeReturn = {
     path: String,
@@ -113,17 +78,38 @@ async function writeToMarkDown(post: Post,markdownPath: string ) : Promise<write
 
 
 
-async function getWpPosts(contentArgs: contentArgs): Promise<Array<Post>> {
-    return new Promise( async (resolve,reject) => {
-        try{
-            const data = await getContent(contentArgs);
-            resolve(data);
-        }catch(error){
-            reject(error);
+async function getWpPosts(args: contentArgs): Promise<Array<Post>> {
+    const {
+        endpoint,
+        perPage,
+    } = args;
+    const page = args.page ? args.page : 1;
+    const postType = args.postType ? args.postType  : 'post';
+    let wp = new WPAPI({ endpoint: endpoint});
+    return new Promise( (resolve,reject) => {
+
+        switch( postType ){
+            case 'page':
+               wp = wp.pages();
+                break;
+            default:
+                wp = wp.posts();
+                break;
         }
+        
+            wp.
+            perPage( perPage )
+            .page( page )
+        
+            .get(function(  err: Error, data: Array<Post> ) {
+            if ( err ) {
+                reject( err );
+            }
+        
+            resolve(data);
+
+        });
     });
-
-
 }
 
 type wpToStaticReturn=  {
