c4d3337c918e2afb1fcc6b1ed67d3fd51b6a0d72
Josh Pollock <josh@calderawp.com>
2019-10-02 13:52:57 -0400

    test other main export

--- packages/wp-api-to-static/src/export.test.ts
+++ packages/wp-api-to-static/src/export.test.ts
@@ -1,5 +1,10 @@
-import {postsToStatic} from './';
+import {postsToStatic,wpToStatic} from './';
 
 test( 'postsToStatic is a function',() => {
     expect( typeof postsToStatic ).toBe( 'function' );
+});
+
+
+test( 'wpToStatic is a function',() => {
+    expect( typeof wpToStatic ).toBe( 'function' );
 });
