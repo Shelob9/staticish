441dbc1ca6e74b37d8b37f43068ae159afb30c1a
Josh Pollock <jpollock412@gmail.com>
2019-12-21 19:51:05 -0500

    tests for single item abstractions

--- packages/wp-api-to-static/src/wp-api/getWpMedia.test.ts
+++ packages/wp-api-to-static/src/wp-api/getWpMedia.test.ts
@@ -1,4 +1,4 @@
-import getWpMedia from './getWpMedia';
+import getWpMedia, { getWpMediaItem } from './getWpMedia';
 
 describe('getWpUsers', () => {
   test('Limits per page', async () => {
@@ -9,5 +9,22 @@ describe('getWpUsers', () => {
       postType: 'media',
     });
     expect(data.length).toBe(2);
+  });
+
+  test('single item', async () => {
+    const medias = await getWpMedia({
+      endpoint: 'http://localhost:8121/wp-json',
+      perPage: 2,
+      page: 1,
+      postType: 'media',
+    });
+
+    const id = medias[1].id;
+    const media = await getWpMediaItem({
+      endpoint: 'http://localhost:8121/wp-json',
+      id,
+    });
+    expect(media.id).toEqual(id);
+    expect(media.link).toEqual(medias[1].link);
   });
 });
--- packages/wp-api-to-static/src/wp-api/getWpUsers.test.ts
+++ packages/wp-api-to-static/src/wp-api/getWpUsers.test.ts
@@ -1,13 +1,29 @@
-import getWpUsers from './getWpUsers';
+import getWpUsers, { getWpUser } from './getWpUsers';
 
-describe('getWpUsers', () => {
+describe('WP API Users', () => {
   test('Limits per page', async () => {
-    const data = await getWpUsers({
+    const users = await getWpUsers({
+      endpoint: 'http://localhost:8121/wp-json',
+      perPage: 2,
+      page: 1,
+      postType: 'user',
+    });
+    expect(users.length).toBe(2);
+  });
+
+  test('Gets single users', async () => {
+    const users = await getWpUsers({
       endpoint: 'http://localhost:8121/wp-json',
       perPage: 3,
       page: 1,
       postType: 'user',
     });
-    expect(data.length).toBe(3);
+    const id = users[1].id;
+    const user = await getWpUser({
+      endpoint: 'http://localhost:8121/wp-json',
+      id,
+    });
+    expect(users[1].slug).toEqual(user.slug);
+    expect(users[1].name).toEqual(user.name);
   });
 });
